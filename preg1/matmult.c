56acf08134f035bc231b860b23265c8843e4e239
chrischriscris <18-10892@usb.ve>
2022-06-08 13:27:13 -0400

    terminada pregunta 1

--- preg1/matmult.c
+++ preg1/matmult.c
@@ -7,12 +7,90 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef unsigned long int ul;
+typedef long int li;
+
+// Separador para imprimir en salida estándar
+char *SEP = "========================================\n";
+
+/**
+ * create_mat - Asigna memoria para matriz vacía, no inicializada de long int.
+ * 
+ * @param n Filas de la matriz.
+ * @param m Columnas de la matriz.
+ * @return Apuntador a la matriz creada.
+ */
+li **create_mat(ul n, ul m) {
+    li **mat = malloc(n * sizeof(li *));
+    for (ul i = 0; i < n; i++)
+        mat[i] = (li *) malloc(m * sizeof(li));
+
+    return mat;
+}
+
+/**
+ * read_mat - Lee una matriz de long int de tamaño nxm de la entrada estándar.
+ * 
+ * @param n: Número de filas de la matriz.
+ * @param m: Número de columnas de la matriz.
+ * @return Apuntador a la matriz leída.
+ */
+li **read_mat(ul n, ul m) {
+    // Asigna memoria para una matriz de n filas y m columnas
+    li **mat = create_mat(n, m);
+
+    for (ul i = 0; i < n; i++)
+        for (ul j = 0; j < m; j++)
+            scanf("%ld", &mat[i][j]);
+
+    return mat;
+}
+/**
+ * mult_mat - Multiplica dos matrices de long int de tamaño nxm y mxp,
+ * respectivamente.
+ * 
+ * @param A: Primera matriz a multiplicar.
+ * @param B: Segunda matriz a multiplicar.
+ * @param n: Número de filas de A.
+ * @param m: Número de columnas de A y filas de B.
+ * @param p: Número de columas de B.
+ * @return: Apuntador a la matriz resultado de la multiplicación.
+ */
+li **mult_mat(li **A, li **B, ul n, ul m, ul p) {
+    // Asigna memoria para la matriz resultante
+    li **C = create_mat(n, p);
+
+    for (ul i = 0; i < n; i++)
+        for (ul j = 0; j < p; j++) {
+            C[i][j] = 0;
+            for (ul k = 0; k < m; k++)
+                C[i][j] += A[i][k] * B[k][j];
+        }
+
+    return C;
+}
+
+/**
+ * Imprime una matriz de enteros de tamaño nxm.
+ * 
+ * @param A: Matriz a imprimir.
+ * @param n: Número de filas de la matriz.
+ * @param m: Número de columnas de la matriz.
+ */
+void print_mat(li **A, ul n, ul m) {
+    for (ul i = 0; i < n; i++) {
+        for (ul j = 0; j < m; j++)
+            printf("%ld\t", A[i][j]);
+        printf("\n");
+    }
+}
+
 /**
  * main - Programa principal.
  *
  * Multiplica dos matrices de tamaño n x m y m x p,
- * imprimendo la matriz resultante de dimensión n x p
- * por salida estándar.
+ * mostrando las matrices a multiplicar y la resultante,
+ * de dimensión n x p, por salida estándar.
  * 
  * Las matrices se proveen por entrada estándar con el
  * siquiente formato:
@@ -26,46 +104,33 @@
  * <b21> <b22> ... <b2p>
  * ...
  * <bm1> <bm2> ... <bmp>
- * 
- * La salida tiene el siguiente formato:
- * <c11> <c12> ... <c1p>
- * <c21> <c22> ... <c2p>
- * ...
- * <cn1> <cn2> ... <cnp>
  */
 int main(int argc, char *argv[]) {
     // Obtiene los tamaños de N, M y P
-    int n = 0, m = 0, p = 0;
-    scanf("%d %d %d", &n, &m, &p);
+    ul n = 0, m = 0, p = 0;
+    scanf("%lu %lu %lu", &n, &m, &p);
 
-    int a[n][m], b[m][p], c[n][p];
+    printf("La dimensión de A es: %lux%lu\n", n, m);
+    printf("La dimensión de B es: %lux%lu\n", m, p);
+    printf(SEP);
 
     // Obtiene las matrices de entrada A y B
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++) 
-            scanf("%d", &a[i][j]);
+    li **A = read_mat(n, m);
+    li **B = read_mat(m, p);
 
-    for (int i = 0; i < m; i++)
-        for (int j = 0; j < p; j++)
-            scanf("%d", &b[i][j]);
+    // Imprime A y B
+    printf("A =\n");
+    print_mat(A, n, m);
+    printf(SEP);
 
-    // Efectúa la multiplicación
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < p; j++) {
-            c[i][j] = 0;
-            for (int k=0; k<m; k++) {
-                c[i][j] += a[i][k] * b[k][j];
-            }
-        }
-    }
+    printf("B =\n");
+    print_mat(B, m, p);
+    printf(SEP);
 
-    // Imprime la matriz resultante
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < p; j++) {
-            printf("%d ", c[i][j]);
-        }
-        printf("\n");
-    }
+    // Calcula e impime la matriz resultante
+    li **C = mult_mat(A, B, n, m, p);
+    printf("C = AxB =\n");
+    print_mat(C, n, p);
 
     return 0;
 }
--- preg1/powermod.c
+++ preg1/powermod.c
@@ -15,7 +15,7 @@ typedef unsigned long int ul;
  * @param a: base
  * @param b: exponente
  * @param c: modulo
- * @returns (a^b) mod c
+ * @return (a^b) mod c
  */
 ul powermod(ul a, ul b, ul c) {
     if (b == 0) return 1;
@@ -25,8 +25,12 @@ ul powermod(ul a, ul b, ul c) {
 /**
  * main - Programa principal.
  *
- * Calcular el valor de (a^b) mod c, donde a, b y c se proporcionan
+ * Calcula el valor de (a^b) mod c, donde a, b y c se proporcionan
  * al programa como argumentos de la línea de comandos.
+ * 
+ * a, b y c deben ser números enteros no negativos, y c debe ser
+ * mayor que 1, en caso contrario el comportamiento del programa
+ * es indefinido.
  */
 int main(int argc, char *argv[]) {
     if (argc != 4) {
@@ -40,9 +44,8 @@ int main(int argc, char *argv[]) {
     b = atol(argv[2]);
     c = atol(argv[3]);
 
-    printf("Calculando powermod(%lu, %lu %lu):", a, b, c);
     ul res = powermod(a, b, c);
-    printf("    Resultado:%lu\n", res);
+    printf("(%lu^%lu) mod %lu = %lu\n", a, b, c, res);
 
     return 0;
 }
