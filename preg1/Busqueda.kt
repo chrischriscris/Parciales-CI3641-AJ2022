1b76bd0ac9cc42ecaf115d039e86078f8d002522
chrischriscris <18-10892@usb.ve>
2022-08-03 11:41:02 -0400

    avances preg 1

--- preg1/Busqueda.kt
+++ preg1/Busqueda.kt
@@ -12,28 +12,31 @@ import kotlin.Double.Companion.POSITIVE_INFINITY
  * Implementación de búsqueda sobre dígrafos.
  * 
  * @param [g]: grafo sobre el que se ejecuta el algoritmo.
- * @param [s]: Entero no negativo que represente el vértice fuente.
  */
 public abstract class Busqueda(val g: Grafo) {
     private var visited = mutableSetOf<Int>()
     abstract val seq: Secuencia<Int>
 
     /**
-     * Realiza un algoritmo
+     * Retorna el número de nodos explorados desde D hasta H, ambos inclusive.
+     * -1 si el vértice H no es alcanzable desde D.
+     *
+     * @throws [RuntimeException] si uno de los vértices dados no pertenece
+     *     al grafo.
      */
     fun buscar(D: Int, H: Int): Int {
         visited = mutableSetOf<Int>()
         var n = 0
-        
-        /* 
+
         if (!g.contains(D))
             throw RuntimeException("El vértice $D no pertenece al grafo.")
         else if (!g.contains(H))
             throw RuntimeException("El vértice $H no pertenece al grafo.")
-        */
+
+        /* Vacía la secuencia */
+        while (!seq.vacio()) seq.remover()
 
         // Algoritmo de búsqueda
-
         // Se añade el primer vértice
         seq.agregar(D)
         while (!seq.vacio()) {
@@ -41,10 +44,7 @@ public abstract class Busqueda(val g: Grafo) {
             
             // El orden lo definirá la implementación de secuencia
             val u = seq.remover()
-            if (u == H) {
-                while (!seq.vacio()) seq.remover()
-                return n
-            }
+            if (u == H) return n
 
             g.adyacentes(u).forEach { if (visited.add(it)) seq.agregar(it) }
         }
@@ -54,10 +54,20 @@ public abstract class Busqueda(val g: Grafo) {
     }
 }
 
+/**
+ * Implementación de la clase abstracta de búsqueda que usa una cola como
+ * la secuencia para añadir y remover los nodos, resultando en una búsqueda
+ * en amplitud.
+ */
 class BFS(g: Grafo) : Busqueda(g) {
     override val seq = Cola<Int>()
 }
 
+/**
+ * Implementación de la clase abstracta de búsqueda que usa una cola como
+ * la secuencia para añadir y remover los nodos, resultando en una bísqueda
+ * en profundidad
+ */
 class DFS(g: Grafo) : Busqueda(g) {
     override val seq = Pila<Int>()
 }
--- preg1/Main.kt
+++ preg1/Main.kt
@@ -2,8 +2,11 @@ import secuencia.*
 import grafo.*
 
 fun main() {
-    println("Prueba de la clase Grafo")
+    println("Test implementaciones de búsqueda:\n")
     val g = Grafo()
+    val dfs = DFS(g)
+    val bfs = BFS(g)
+
     g.agregarVertice(0)
     g.agregarVertice(13)
     g.agregarVertice(14)
@@ -19,16 +22,29 @@ fun main() {
     g.agregarLado(9, 10)
     g.agregarLado(11, 9)
 
-    println("DFS:")
-    var dfs = DFS(g)
-    println("Total de nodos: ${dfs.buscar(1, 2)}")
-    println("Total de nodos: ${dfs.buscar(1, 10)}")
-    println("Total de nodos: ${dfs.buscar(1, 14)}")
+    println("Haciendo DFS:\n")
+    println("dfs.buscar(1, 2) = ${dfs.buscar(1, 2)}")
+    println("dfs.buscar(1, 10) = ${dfs.buscar(1, 10)}")
+    println("dfs.buscar(1, 14) = ${dfs.buscar(1, 14)}")
 
+    println("\nHaciendo BFS:\n")
+    println("bfs.buscar(1, 2) = ${bfs.buscar(1, 2)}")
+    println("bfs.buscar(1, 10) = ${bfs.buscar(1, 10)}")
+    println("bfs.buscar(1, 14) = ${bfs.buscar(1, 14)}")
 
-    println("BFS:")
-    var bfs = BFS(g)
-    println("Total de nodos: ${dfs.buscar(1, 2)}")
-    println("Total de nodos: ${bfs.buscar(1, 10)}")
-    println("Total de nodos: ${bfs.buscar(1, 14)}")
+    println("\nTest de excepciones\n")
+    try {
+        print("bfs.buscar(1, 100) = ")
+        println("${bfs.buscar(1, 100)}")
+    }
+    catch(e: RuntimeException) {
+        println("Excepción atajada: ${e.message}")
+    }
+    try {
+        print("dfs.buscar(-10, 1) = ")
+        println("${bfs.buscar(-10, 1)}")
+    }
+    catch(e: RuntimeException) {
+        println("Excepción atajada: ${e.message}")
+    }
 }
